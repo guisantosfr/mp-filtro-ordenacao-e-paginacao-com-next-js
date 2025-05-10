c53af6b7f9c4380050b45747e4f37d59ceecacd8
Guilherme Santos <santosgui678@gmail.com>
2025-05-10 11:19:50 -0300

    Populando tabela de pedidos com dados da API

--- components/orders-table.tsx
+++ components/orders-table.tsx
@@ -9,14 +9,26 @@ import {
 import { Badge } from './ui/badge';
 import { ChevronsUpDown } from 'lucide-react';
 
-export default function OrdersTable() {
+export default async function OrdersTable() {
+  const data = await fetch(process.env.API_URL + '/orders-api/orders');
+  const orders = await data.json();
+
+  const formatAmount = (amount: number) => {
+    const amountInR$ = (amount / 100).toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    return amountInR$;
+  };
+
   return (
     <Table>
       <TableHeader>
         <TableRow className="w-full">
           <TableHead className="table-cell">Cliente</TableHead>
           <TableHead className="table-cell">Status</TableHead>
-          <TableHead className="table-cell cursor-pointer justify-end items-center gap-1">
+          <TableHead className="hidden md:table-cell cursor-pointer justify-end items-center gap-1">
             <div className="flex items-center gap-1">
               Data
               <ChevronsUpDown className="w-4" />
@@ -29,36 +41,30 @@ export default function OrdersTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Fulano de Tal</div>
-            <div className="hidden md:inline text-sm text-muted-foreground">
-              fulano.de.tal@gmail.com
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge className={`text-xs`} variant="outline">
-              Pendente
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2024-01-01</TableCell>
-          <TableCell className="text-right">R$100,00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Ciclana de Tal</div>
-            <div className="text-sm text-muted-foreground">
-              ciclana.de.tal@gmail.com
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge className={`text-xs`} variant="outline">
-              Completo
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-01-01</TableCell>
-          <TableCell className="text-right">R$500,00</TableCell>
-        </TableRow>
+        {
+          orders.data.map(order => {
+            return (
+              <TableRow key={order.id}>
+                <TableCell>
+                  <div className="font-medium">{order.customer_name}</div>
+                  <div className="hidden md:inline text-sm text-muted-foreground">
+                    {order.customer_email}
+                  </div>
+                </TableCell>
+
+                <TableCell>
+                  <Badge className={`text-xs`} variant="outline">
+                    {order.status === 'completed' ? 'Completo' : 'Pendente'}
+                  </Badge>
+                </TableCell>
+
+                <TableCell className="hidden md:table-cell">{order.order_date}</TableCell>
+
+                <TableCell className="text-right">{formatAmount(order.amount_in_cents)}</TableCell>
+              </TableRow>
+            )
+          })
+        }
       </TableBody>
     </Table>
   );
